838fc5e92d005a2db435aea4386998313f127636
adrjo <adrianjohansson16@gmail.com>
2025-10-14 17:10:59 +0200

    feat: setup expo router tabs skeleton

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,37 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="favorites"
+                options={{
+                    title: "Favorites",
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons name={focused ? "bookmarks" : "bookmarks-outline"} color={color} size={size} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons name={focused ? "home" : "home-outline"} color={color} size={size} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons name={focused ? "build" : "build-outline"} color={color} size={size} />
+                    )
+                }}
+            />
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+        </Tabs>
+    )
 }
