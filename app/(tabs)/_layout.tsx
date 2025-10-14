3d9b43447a4023e447b93f226881690e9a5a4bb6
adrjo <adrianjohansson16@gmail.com>
2025-10-14 17:39:26 +0200

    style: add home tab styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
     return (
-        <Tabs>
+        <Tabs screenOptions={{ headerShown: false }}>
             <Tabs.Screen
                 name="favorites"
                 options={{
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,75 @@
-import { Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const [searchInput, setSearch] = useState("");
+
+  const onGpsClick = () => {
+    console.log("click");
+  }
+
+  const onSubmit = () => {
+    console.log(searchInput);
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.outer}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Sök på en stad eller address..."
+            onChangeText={setSearch}
+            onSubmitEditing={onSubmit}
+          />
+          <TouchableOpacity style={styles.gps} onPress={onGpsClick}>
+            <Ionicons name="location-outline" size={32} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  outer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin:15,
+  },
+  container: {
+    width: '100%',
+    alignItems:'center',
+  },
+  textContainer: {
+    borderRadius: 15,
+    backgroundColor: 'white',
+    padding: 20,
+    margin: 10,
+    elevation: 3,
+    //ios
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: .2,
+    shadowRadius: 3,
+
+    maxWidth: 400,
+    width: '100%',
+
+    flex: 1,
+    flexDirection: 'row'
+  },
+  input: {
+    width: '90%',
+    padding:'auto',
+  },
+  gps: {
+    marginRight: 0,
+    margin: 'auto'
+  }
+})
