c8d1cab825887f7e1d358c631e997d4bb650ae71
adrjo <adrianjohansson16@gmail.com>
2025-10-15 15:41:38 +0200

    fix: set initial route to index page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs initialRouteName="index" screenOptions={{ headerShown: false }}>
             <Tabs.Screen
                 name="favorites"
                 options={{
